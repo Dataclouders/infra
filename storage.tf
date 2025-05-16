7174124c79103f32d62c88fd4d049c734f40b2d1
David Acevedo <dacevedoa12@gmail.com>
2025-05-16 11:36:39 -0500

    Nueva base de datos

--- storage.tf
+++ storage.tf
@@ -1,3 +1,30 @@
 resource "aws_s3_bucket" "dataclouders-infra-demo" {
   bucket = "dataclouders-infra-demo-github"
 }
+
+resource "aws_s3_bucket" "dataclouders-terraform-states" {
+  bucket = "dataclouders-terraform-states"
+}
+
+resource "aws_dynamodb_table" "productos" {
+  name           = "productos"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "pk"
+  range_key      = "sk"
+
+  attribute {
+    name = "pk"
+    type = "S"
+  }
+  attribute {
+    name = "sk"
+    type = "S"
+  }
+
+  globasdasdasdasdal_secondary_index {
+    name = "inverted-index"
+    hash_key = "sk"
+    range_key = "pk"
+    projection_type = "ALL"
+  }
+}
